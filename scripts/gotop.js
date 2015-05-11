c711ce19ed024ec85917fc93ea002e7b061cda22
QiannanXu <qnxu@thoughtworks.com>
2015-05-11 22:03:12 +0530

    modify

---
+++ scripts/gotop.js
@@ -0,0 +1,16 @@
+<script type="text/javascript">
+$(function(){
+    $(".gotop").click(function(){
+        jQuery("html,body").animate({
+            scrollTop:0
+        },1000);
+    });
+    $(window).scroll(function() {
+        if ( $(this).scrollTop() > 300){
+            $('.gotop').fadeIn("fast");
+        } else {
+            $('.gotop').stop().fadeOut("fast");
+        }
+    });
+});
+</script>
